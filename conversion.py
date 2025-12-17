3471be8cd8a18db8fff765988abd798b21814b65
Soumya-RanjanBhoi <soumya6ranjan9@gmail.com>
2025-12-17 19:27:34 +0530

    added data ingestion part

---
+++ conversion.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+from pathlib import Path
+
+async def convert_to_pdf(file_path: str, output_dir: str) -> Path:
+    try:
+        SOFFICE_PATH = r"C:\Program Files\LibreOffice\program\soffice.exe"
+        
+        subprocess.run([
+            SOFFICE_PATH,
+            "--headless",
+            "--convert-to", "pdf",
+            "--outdir", output_dir,
+            file_path
+        ], check=True, capture_output=True)
+
+        original_stem = Path(file_path).stem
+        pdf_path = Path(output_dir) / f"{original_stem}.pdf"
+        
+        if pdf_path.exists():
+            return pdf_path
+        else:
+            raise FileNotFoundError("Conversion finished but PDF not found.")
+    
+    except Exception as e:
+        print(f"Conversion Error: {e}")
+        raise
--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-  
+import os
+import shutil
+from pathlib import Path
+from fastapi import FastAPI, File, UploadFile
+from conversion import convert_to_pdf 
+
+app = FastAPI()
+
+UPLOAD_DIR = Path("upload_files")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+async def save_file(file: UploadFile):
+    file_path = UPLOAD_DIR / file.filename
+    
+    try:
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        return file_path
+    except Exception as e:
+        print(f"Unable to save file: {e}")
+        raise
+
+@app.post("/process")
+async def processing(file: UploadFile = File(...)):
+    try:
+        saved_path = await save_file(file)
+        
+        if file.filename.lower().endswith(".pdf"):
+            final_path = saved_path
+        else:
+            final_path = await convert_to_pdf(str(saved_path), str(UPLOAD_DIR))
+            saved_path.unlink()
+        
+        return {
+            "status": "success",
+            "pdf_path": str(final_path)
+        }
+    except Exception as e:
+        return {"error": str(e)}
